1d7c8d9e6d7a71f5d6c750a6494feb9ac9eaa8d6
igorgvozden <igorgvozden@yahoo.com>
2020-08-06 09:36:40 +0200

    Updates

---
+++ precache-manifest.8c971e7c601702b8c4059dc8381c0386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3b2c21abc5cbcfe01988beb0fa47a6",
+    "url": "/react-movie-search/index.html"
+  },
+  {
+    "revision": "d34cbdb7d6f18046a138",
+    "url": "/react-movie-search/static/css/main.7f762644.chunk.css"
+  },
+  {
+    "revision": "ccf3d6415941bd8389cb",
+    "url": "/react-movie-search/static/js/2.73358fcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie-search/static/js/2.73358fcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d34cbdb7d6f18046a138",
+    "url": "/react-movie-search/static/js/main.0a0725cc.chunk.js"
+  },
+  {
+    "revision": "43c8c52bca64ef8827a2",
+    "url": "/react-movie-search/static/js/runtime-main.d27a8576.js"
+  }
+]);
